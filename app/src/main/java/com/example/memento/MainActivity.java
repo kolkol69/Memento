4c4308ab057fbc2d326996af3d4ce5e7ff46bc1a
kolkol <kolodiy1103@gmail.com>
2019-11-21 01:39:12 +0100

    Add menu with sample button and a checkbox + change menu button color to white to be in the same color style as everything else

--- app/src/main/java/com/example/memento/MainActivity.java
+++ app/src/main/java/com/example/memento/MainActivity.java
@@ -6,6 +6,8 @@ import androidx.fragment.app.FragmentManager;
 import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.example.memento.adapter.TabAdapter;
 import com.google.android.material.tabs.TabLayout;
@@ -13,16 +15,42 @@ import com.google.android.material.tabs.TabLayout;
 public class MainActivity extends AppCompatActivity {
 
     FragmentManager fragmentManager;
+    PreferenceHelper preferenceHelper;
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        MenuItem splashItem = menu.findItem(R.id.action_splash);
+        splashItem.setChecked(preferenceHelper.getBoolean(PreferenceHelper.SPLASH_IS_INVISIBLE));
+        return true;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         setTheme(R.style.AppTheme);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        PreferenceHelper.getInstance().init(getApplicationContext());
+        preferenceHelper = PreferenceHelper.getInstance();
         
         fragmentManager = getSupportFragmentManager();
 
         setUI();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        int id = item.getItemId();
+
+        if (id == R.id.action_splash) {
+            item.setChecked(!item.isChecked());
+            preferenceHelper.putBoolean(PreferenceHelper.SPLASH_IS_INVISIBLE, item.isChecked());
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
     private void setUI() {
